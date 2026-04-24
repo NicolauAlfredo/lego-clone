d602123fe797fadb090447ab7b195263860afa57
Federico Maria Calato <calatofederico48@gmail.com>
2026-04-24 22:46:05 +0200

    aggiunto funzione js su file favorites.js per toggle dei preferiti

---
+++ js/features/favorites.js
@@ -0,0 +1,8 @@
+function togglePreferito(id) {
+  for (let i = 0; i < CardProduct.length; i++) {
+    if (CardProduct[i].id === id) {
+      CardProduct[i].favorite = !CardProduct[i].favorite;
+      break;
+    }
+  }
+}
