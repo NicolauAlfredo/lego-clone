ef0dc883595eedba475acb2f2150732b716e0698
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-04-27 17:06:53 +0200

    test(js): add validation script for Set, Cart and Favorites logic

--- js/features/favorites.js
+++ js/features/favorites.js
@@ -1,4 +1,4 @@
-function togglePreferito(id) {
+export function togglePreferito(id) {
   for (let i = 0; i < CardProduct.length; i++) {
     if (CardProduct[i].id === id) {
       CardProduct[i].favorite = !CardProduct[i].favorite;
