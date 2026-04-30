88aefd9582ebddaac534667fb00fa244093df9dc
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-04-30 18:33:39 +0200

    Add comment to currency formatting utility

--- js/utils/currency.js
+++ js/utils/currency.js
@@ -1,5 +1,6 @@
 // Formatta un valore numerico in valuta euro (formato italiano)
 // Esempio: 89.9 → "89,90 €"
 export function formatCurrency(value) {
+  // Case decimale (quantitÀ di numero dopo la virgola = toFixed(2) 2 = alla quantità di numeri dopo la virgula)
   return `${value.toFixed(2).replace(".", ",")} €`;
 }
