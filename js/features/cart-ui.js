ab58e36778888548c54507de56e9792505c33f91
Rosario Floridia <rosariofloridia35@gmail.com>
2026-05-10 11:18:06 +0200

    adjusted errors in cart-ui

--- js/features/cart-ui.js
+++ js/features/cart-ui.js
@@ -1,22 +1,46 @@
-import {Cart} from "../models/Cart.js";
-import {Product} from "../models/Product.js";
+import { Cart } from "../models/Cart.js";
+import { Product } from "../models/Product.js";
 
 const carrello = new Cart();
 
-const contatoreCarrello = document.querySelectorAll(".mobile-header__cart-count, .header__cart-count");
+const contatoreCarrello = document.querySelectorAll(
+  ".mobile-header__cart-count, .header__cart-count",
+);
+
 const carosello = document.querySelector(".perfect-set-carousel");
 
-
 function updateCartCounter() {
-    const contatoreAggiornato = Cart.countProduct();
+  const contatoreAggiornato = carrello.countProducts();
 
+  contatoreCarrello.forEach((counter) => {
+    const isDesktopCounter = counter.classList.contains("header__cart-count");
 
-contatoreCarrello.forEach((counter) => {
-    if (counter.classList.contains(".header__cart-count")){
-        counter.textContent = contatoreAggiornato;
-    } else {
-        counter.textContent = contatoreAggiornato;
-    }
-});
+    counter.textContent = isDesktopCounter
+      ? `(${contatoreAggiornato})`
+      : contatoreAggiornato;
+  });
 }
+
+function aggiungiAlCarrello(event) {
+    console.log("Sono entrato nel aggiungi al carrelo")
+  const button = event.target.closest("[data-add-to-cart]");
+
+  if (!button) return;
+
+  const prodotto = new Product({
+    id: button.dataset.productId,
+    name: button.dataset.productName,
+    price: Number(button.dataset.productPrice),
+    image: button.dataset.productImage,
+  });
+
+  carrello.addProduct(prodotto);
+
+  updateCartCounter();
+}
+
+if (carosello) {
+  carosello.addEventListener("click", aggiungiAlCarrello);
+}
+
 updateCartCounter();
