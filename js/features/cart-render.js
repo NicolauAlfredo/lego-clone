ace16c97b49169ab15895fa9deb2b31e285f48cd
Rosario Floridia <rosariofloridia35@gmail.com>
2026-05-13 17:57:59 +0200

    bug fixes

--- js/features/cart-render.js
+++ js/features/cart-render.js
@@ -6,6 +6,9 @@ const cartTitle = document.querySelector("[data-cart-title]");
 
 const cartContainer = document.querySelector("[data-cart-products]")
 
+const cartCounters = document.querySelectorAll(
+  ".mobile-header__cart-count, .header__cart-count",
+);
 
 const cartIva = document.querySelector("[data-order-iva]")
 const cartOrderItems = document.querySelector("[data-order-items]")
@@ -50,13 +53,13 @@ function CartCardProduct(product){
             <div class="cart_bottoni-wrapper">
               <div class="cart_bottoni">
                 <div class="qty-btn-minus">
-                  <button class="qty-btn btn-minus" data-btn-minus></button>
+                  <button class="qty-btn btn-minus" data-btn-minus type="button" data-id="${product.id}"></button>
                 </div>
 
                 <span class="qty-num">${product.quantity}</span>
 
                 <div class="qty-btn-plus">
-                  <button class="qty-btn btn-plus" data-btn-plus></button>
+                  <button class="qty-btn btn-plus" data-btn-plus type="button" data-id="${product.id}"></button>
                 </div>
               </div>
 
@@ -80,9 +83,10 @@ function CartCardProduct(product){
 }
 
 // Calcola il prezzo totale del prodotto
-const totalPrice = products.reduce((total, product) => {
+function calculateTotalPrice (products){ 
+    return products.reduce((total, product) => {
         return total + product.price * product.quantity
-    }, 0)
+    }, 0)}
 
 // Calcola il numero dei prodotti
 function calculateTotalProducts(products) {
@@ -91,8 +95,22 @@ function calculateTotalProducts(products) {
     }, 0);
 }
 
+// Update il numero della quantità nel carrello
+function updateCartCounters(products) {
+  const totalItems = calculateTotalProducts(products);
+
+  cartCounters.forEach((counter) => {
+    if (counter.classList.contains("header__cart-count")) {
+      counter.textContent = `(${totalItems})`;
+    } else {
+      counter.textContent = totalItems;
+    }
+  });
+}
+
 //Add to card e remove to card
 function increaseProductQuantiy(productId){
+  const products = getCartProducts()
   const updateProducts = products.map((product) => {
     if (product.id === productId){
       return {
@@ -103,21 +121,26 @@ function increaseProductQuantiy(productId){
     return product
   })
   saveCart(updateProducts)
+
   renderCartProducts()
 }
 
 function decreaseProductQuantiy(productId){
+  const products = getCartProducts()
   const updateProducts = products.map((product) => {
     if (product.id === productId){
+      if (product.quantity === 1){
+        return product
+      }
       return {
         ...product,
         quantity: product.quantity - 1,
       }
     }
     return product
-  }).filter((product)=> {
-    product.quantity > 0
-  })
+  }).filter((product) => product.quantity > 0
+  )
+
   saveCart(updateProducts)
   renderCartProducts()
 }
@@ -125,20 +148,23 @@ function decreaseProductQuantiy(productId){
 
 // Funzione di rendering
 function renderCartProducts(){
-
-    const product = getCartProducts();
+    const products = getCartProducts();
     const totalProdcuts = calculateTotalProducts(products);
+    const totalPrice = calculateTotalPrice(products)
     const iva = totalPrice * 0.22;
+
+    updateCartCounters(products)
 
     if (!cartContainer){
         return
     } 
-    if(totalProdcuts.length === 0){
+    if(totalProdcuts === 0){
         cartTitle.textContent =  `Il mio carrello è vuoto`
         cartContainer.innerHTML = ""
         cartOrderItems.textContent =  `Valore ordine (0) articoli `
         cartOrderSubtotal.textContent = formatCurrency(0)
         cartOrderTotal.textContent = formatCurrency(0)
+        cartIva.textContent = `Include IVA ${formatCurrency(0)}`
     }
 
     cartTitle.textContent = `Il mio carrello (${totalProdcuts})`
@@ -147,23 +173,23 @@ function renderCartProducts(){
     cartOrderTotal.textContent = formatCurrency(totalPrice);
     cartIva.textContent =  `Include IVA ${formatCurrency(iva)} `
     cartContainer.innerHTML = products.map(CartCardProduct).join("")
+    
   }
 
 
   cartContainer.addEventListener("click", (event) => {
-    const increaseProdcut = document.querySelector("[data-btn-plus]")
-    const decreaseProduct = document.querySelector("[data-btn-minus]")
+    const increaseProdcut = event.target.closest("[data-btn-plus]")
+    const decreaseProduct = event.target.closest("[data-btn-minus]")
 
     if (increaseProdcut) {
       const productId = increaseProdcut.dataset.id
       increaseProductQuantiy(productId)
+
     }
     if (decreaseProduct) {
       const productId = decreaseProduct.dataset.id
       decreaseProductQuantiy(productId)
     }
-
-
   })
 
 console.table(products)
