102afd9ae1b10f0406c878002a063255fd71bb21
Felice556 <felice.lgg@gmail.com>
2026-05-18 14:40:23 +0200

    aggiunto bottone guarda subito e aggiustato l'hover delle cards con javascript

---
+++ cards.js
@@ -0,0 +1,27 @@
+const allCards = document.querySelectorAll('.cards a');
+
+allCards.forEach((card, index) => {
+  card.addEventListener('mouseenter', () => {
+    // reset tutto
+    allCards.forEach(c => c.style.transform = 'scale(1)');
+    
+    
+    // card principale
+    card.style.transform = 'scale(1.15)';
+     card.style.zIndex = '10';
+    
+    // card precedente
+    if (allCards[index - 1]) {
+      allCards[index - 1].style.transform = 'scale(1.1)';
+    }
+    
+    // card successiva
+    if (allCards[index + 1]) {
+      allCards[index + 1].style.transform = 'scale(1.1)';
+    }
+  });
+
+  card.addEventListener('mouseleave', () => {
+    allCards.forEach(c => c.style.transform = 'scale(1)');
+  });
+});
